6b0b419e848ecdca86fa38f75118de7eb1b73201
Patrick Yee <pyee@23andme.com>
2023-11-10 10:08:14 -0800

    treesitter, telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,10 +10,6 @@ require("packer").startup(function()
   use "vim-airline/vim-airline"
   use "vim-airline/vim-airline-themes"
   use "drewtempelmeyer/palenight.vim"
-  use "junegunn/fzf"
-  use "junegunn/fzf.vim"
-  use "tpope/vim-fugitive"
-  use "airblade/vim-gitgutter"
   use "djoshea/vim-autoread"
   use {
     "neoclide/coc.nvim", branch="release", config=function()
@@ -30,6 +26,12 @@ require("packer").startup(function()
     requires = { "tyru/open-browser.vim" }
   }
   use "christoomey/vim-tmux-navigator"
+  use "APZelos/blamer.nvim"
+  use {
+    "nvim-telescope/telescope.nvim",
+    requires = { "nvim-lua/plenary.nvim" }
+  }
+  use "nvim-treesitter/nvim-treesitter"
 end)
 
 -- theme and color
@@ -88,12 +90,16 @@ vim.api.nvim_create_autocmd(
 -- mouse support
 vim.cmd [[set mouse=r]]
 
+-- enable treesitter
+require "nvim-treesitter.configs".setup {
+  highlight = {
+    enable = true,
+  },
+}
+
 -- shortcuts
 vim.g.mapleader = " "
 vim.api.nvim_set_keymap("n", "<leader>n", ":NERDTreeToggle<cr>", {})
-vim.api.nvim_set_keymap("n", "<leader>b", ":Buffers<cr>", {})
-vim.api.nvim_set_keymap("n", "<leader>o", ":FZF<cr>", {})
-vim.api.nvim_set_keymap("n", "<leader>f", ":Rg ", {})
 vim.api.nvim_set_keymap("v", "<leader>r", ":'<,'>OpenGithubFile<cr>", {})
 vim.api.nvim_set_keymap("n", "<leader>r", ":OpenGithubFile<cr>", {})
 vim.api.nvim_set_keymap("n", "<Tab>", "<C-w>w", {})
@@ -104,3 +110,8 @@ vim.api.nvim_set_keymap("n", "<C-H>", "<C-w><C-H>", {})
 vim.api.nvim_set_keymap("n", "<leader>d", "<Plug>(coc-definition)", {})
 vim.api.nvim_set_keymap("n", "<leader>u", "<Plug>(coc-references)", {})
 vim.api.nvim_set_keymap("v", "<leader>y", '"+y', {})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>o", builtin.find_files, {})
+vim.keymap.set("n", "<leader>f", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>b", builtin.buffers, {})
+vim.keymap.set("n", "<leader>h", builtin.help_tags, {})
