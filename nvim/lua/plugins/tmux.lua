2dfad3daccd877bfb2cc4f271fcd29bd1db6f308
Patrick Yee <pyee@23andme.com>
2026-07-04 18:15:14 -0700

    tmux enhance

--- nvim/lua/plugins/tmux.lua
+++ nvim/lua/plugins/tmux.lua
@@ -1,5 +1,25 @@
 return {
   {
     "christoomey/vim-tmux-navigator",
+    init = function()
+      -- don't let the plugin install its default <C-h/j/k/l> mappings;
+      -- we define them explicitly below and keep <C-l> for clear-highlight
+      vim.g.tmux_navigator_no_mappings = 1
+    end,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "Navigate left (tmux)" },
+      { "<c-j>", "<cmd>TmuxNavigateDown<cr>", desc = "Navigate down (tmux)" },
+      { "<c-k>", "<cmd>TmuxNavigateUp<cr>", desc = "Navigate up (tmux)" },
+      { "<c-l>", "<cmd>TmuxNavigateRight<cr>", desc = "Navigate right (tmux)" },
+      { "<c-\\>", "<cmd>TmuxNavigatePrevious<cr>", desc = "Navigate to previous (tmux)" },
+      { "<leader>l", "<cmd>nohlsearch<cr>", desc = "Clear search highlight" },
+    },
   },
 }
